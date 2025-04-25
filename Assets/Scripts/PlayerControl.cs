9df6fc99eec318ce2810a34b187ce93faedb2542
sonia92ab <156215121+sonia92ab@users.noreply.github.com>
2025-04-25 15:34:08 +0330

    Input project

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,28 @@
+
+using System.Linq.Expressions;
+using System.Numerics;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public float moveforce = 10f;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var HorizontalInput= Input.GetAxis("Horizontal");
+        UnityEngine.Vector2 force = new UnityEngine.Vector2(HorizontalInput * moveforce, 0);
+        rb.AddForce(force); 
+}
+}
